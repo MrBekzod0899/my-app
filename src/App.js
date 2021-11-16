a19431a541166b5c866e15eed1394ed49972a95f
MrBekzod0899 <bekzodqurbonov1399@gmail.com>
2021-11-16 14:00:00 +0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NavbarHeader1 from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
+import {Routes,Route,BrowserRouter,Switch} from "react-router-dom"
+import Content from './Components/Content/Content'
+import Footer from './Components/Footer/Footer'
+import SchoolLive from './Components/MaktabHayoti/SchoolLive'
+import Contact from './Components/Contact/Contact'
+import Darsliklar from './Components/Darsliklar/Darsliklar'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-          heloo woorldd  adfsdfdaf
+           <Switch>
+                  <Route exact path="/">
+                        <NavbarHeader1/>
+                        <Content/>
+                        <Contact/>
+                        <Darsliklar/>
+                        <Footer/>
+                  </Route>
+                  <Route exact path="/maktab-hayoti">
+                    <NavbarHeader1/>
+                    <SchoolLive/>
+                    <Footer/>
+                  </Route>
+          </Switch>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter,Route } from 'react-router-dom';
+import 'aos'
+import "aos/dist/aos.css";
+import '@mui/material'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+       <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
